2894f92cdddf0beb8d9f2a6669fb671af8518acc
cheroliv <cheroliv@users.noreply.github.com>
2026-07-06 19:40:38 +0200

    chore(cnv): bump education.cccp.build.* 0.0.1 -> 0.0.2 (harmonisation conventions-gradle)

--- api-key-pool-plugin/build.gradle.kts
+++ api-key-pool-plugin/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     `java-library`
     `maven-publish`
     alias(libs.plugins.kotlin.jvm)
-    id("education.cccp.build.publishing") version "0.0.1"
+    id("education.cccp.build.publishing") version "0.0.2"
 }
 
 group = "education.cccp"
